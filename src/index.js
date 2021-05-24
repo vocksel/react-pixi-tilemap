d751da5f6f37b98fba357348a701c951bf57da77
David Minnerly <me@vocksel.com>
2021-05-24 15:26:39 -0400

    Removing other imports fixes it??

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import Tilemap from './Tilemap'
-import useCollisions from './useCollisions'
-import useSpawnPoint from './useSpawnPoint'
 
 export {
     Tilemap,
-    useCollisions,
-    useSpawnPoint,
 }
