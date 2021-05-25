e97db095c754ba2e6b7d6ef80fc234f84bece6f1
David Minnerly <me@vocksel.com>
2021-05-24 20:09:12 -0400

    Fix collisions not being propagated

--- src/useCollisions.js
+++ src/useCollisions.js
@@ -1,14 +1,13 @@
 import { Rectangle } from "pixi.js"
-import { useContext, useState, useEffect } from "react"
+import { useContext, useMemo } from "react"
 import TilemapContext from "./TilemapContext"
 
 const useCollisions = () => {
     const { map } = useContext(TilemapContext)
-    const [collisions, set] = useState([])
 
-    useEffect(() => {
-        const collisionRects = map?.layers
-            .filter(layer => layer.tiles === null)    
+    const collisions = useMemo(() => {
+        return map.layers
+            .filter(layer => layer.type === 'tile')    
             .map(layer => {
                 const rects = []
         
@@ -28,11 +27,6 @@ const useCollisions = () => {
                 return rects
             })
             .flat()
-        
-        const newCollisions = collisionRects.map((rect, index) =>
-            <Sprite key={index} texture={Texture.WHITE} position={new Point(rect.x, rect.y)} width={rect.width} height={rect.height} />)
-
-        set(newCollisions)
     }, [ map ])
 
     return collisions
