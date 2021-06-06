1a9e60323e7103f0c8d507a1cd31db5f87d0249e
David Minnerly <voxeldavid@gmail.com>
2021-06-05 20:13:21 -0400

    Expose useInteractables

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import useTilemapLoader from './useTilemapLoader'
 import useMap from './useMap'
 import useSpawnPoint from './useSpawnPoint'
 import useCollisions from './useCollisions'
+import useInteractables from './useInteractables'
 import useObjects from './useObjects'
 import useTiles from './useTiles'
 
 export {
     Tilemap,
     useTilemapLoader,
+    useInteractables,
     useMap,
     useSpawnPoint,
     useCollisions,
