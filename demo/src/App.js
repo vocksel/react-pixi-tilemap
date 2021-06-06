20411a9825acc81467b0c33fe34aa5f92ed8094e
David Minnerly <voxeldavid@gmail.com>
2021-06-05 20:12:51 -0400

    Add a default if there's no foreground layer

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,4 +1,5 @@
-import { Stage } from '@inlet/react-pixi';
+import { Stage, Sprite } from '@inlet/react-pixi';
+import { Texture } from '@pixi/core';
 import { Tilemap, useTilemapLoader } from 'react-pixi-tilemap'
 
 const tilemape = process.env.PUBLIC_URL + '/stages/map.tmx'
@@ -8,7 +9,12 @@ const App = () => {
 
     return (
         <Stage width={window.innerWidth} height={window.innerHeight} options={{ resizeTo: window }}>
-            <Tilemap map={map} scale={0.75} />
+            <Tilemap map={map} scale={0.75}>
+                {/* These sprites show off the layering order */}
+                <Sprite texture={Texture.WHITE} x={120} y={380} width={64} height={64} />
+                <Sprite texture={Texture.WHITE} x={700} y={420} width={64} height={64} />
+                <Sprite texture={Texture.WHITE} x={500} y={700} width={64} height={64} />
+            </Tilemap>
         </Stage>
     )
 }
--- src/Tilemap.jsx
+++ src/Tilemap.jsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import { Container } from '@inlet/react-pixi'
 import TilemapContext from './TilemapContext'
 import Layer from './Layer'
 
 const Tilemap = ({ map, children, ...props }) => {
+    const [hasForeground, setHasForeground] = useState(false)
+
     const layers = useMemo(() => {
         return map?.layers.map((layer, index) => {
             const { name } = layer
             if (name === 'Foreground') {
+                setHasForeground(true)
                 return children
             } else {
                 return <Layer key={index} layer={layer} map={map} />
@@ -15,20 +18,15 @@ const Tilemap = ({ map, children, ...props }) => {
         })
     }, [ map, children ])
 
-    useEffect(() => {
-        if (children) {
-            if (map?.layers.find(layer => layer.name === 'Foreground') === null) {
-                throw new Error(`Tilemap has children specified, but no 'Foreground' layer. You need a Foreground layer for the children to render.`)
-            }
-        }
-    }, [ map ])
-
     // Only render once the map is loaded so that our hooks don't need to
     // conditionally check if anything in TilemapContext exists.
     if (map) {
         return <TilemapContext.Provider value={{ map }}>
             <Container {...props}>
                 {layers}
+
+                {/* If there's no foreground layer specified in the map, we simply render the children on top */}
+                {!hasForeground && children}
             </Container>
         </TilemapContext.Provider>
     } else {
